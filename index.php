b59ffde1c43ef443524bee318cb3670c9d45d1a4
Nicholas Glantz <nike18054@gmail.com>
2017-08-23 15:19:24 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>League Guides </title>
+    <link rel="icon" href="#">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="League Guides will help you build to your playstyle. We offer quick video tutorials and in depth guides to the champions you love to play.">
+
+    <!--bootsrap css-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    <!-- JAVASCRIPT files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </head>
+
+  <body>
+
+
+
+  </body>
+</html>
